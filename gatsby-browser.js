67524b22f9288c2f447047865845bca17f12c2a3
Manjula Dube <dube.manjula668@gmail.com>
2019-04-30 22:53:55 +0200

    add somme

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,3 +7,4 @@
 // You can delete this file if you're not using it
 import wrapWithProvider from "./wrap-with-provider"
 export const wrapRootElement = wrapWithProvider
+require("prismjs/themes/prism-okaidia.css")
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'React Advance Workshop',
+    title: 'React Advance Workshop'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -8,28 +8,28 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        path: `${__dirname}/src/images`
+      }
     },
     {
       resolve: `gatsby-transformer-json`,
       options: {
-        typeName: `MenuItems`, // a fixed string
-      },
+        typeName: `MenuItems` // a fixed string
+      }
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `menuItems`,
-        path: `${__dirname}/src/menuItems`,
-      },
+        path: `${__dirname}/src/menuItems`
+      }
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `contents`,
-        path: `${__dirname}/contents`,
-      },
+        path: `${__dirname}/contents`
+      }
     },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
@@ -42,8 +42,8 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
-      },
+        icon: 'src/images/gatsby-icon.png' // This path is relative to the root of the site.
+      }
     },
     {
       resolve: `gatsby-transformer-remark`,
@@ -53,8 +53,44 @@ module.exports = {
           {
             resolve: `gatsby-remark-autolink-headers`,
             options: {
-              className: 'post-toc-anchor',
-            },
+              className: 'post-toc-anchor'
+            }
+          },
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              // Class prefix for <pre> tags containing syntax highlighting;
+              // defaults to 'language-' (eg <pre class="language-js">).
+              // If your site loads Prism into the browser at runtime,
+              // (eg for use with libraries like react-live),
+              // you may use this to prevent Prism from re-processing syntax.
+              // This is an uncommon use-case though;
+              // If you're unsure, it's best to use the default value.
+              classPrefix: 'language-',
+              // This is used to allow setting a language for inline code
+              // (i.e. single backticks) by creating a separator.
+              // This separator is a string and will do no white-space
+              // stripping.
+              // A suggested value for English speakers is the non-ascii
+              // character '›'.
+              inlineCodeMarker: null,
+              // This lets you set up language aliases.  For example,
+              // setting this to '{ sh: "bash" }' will let you use
+              // the language "sh" which will highlight using the
+              // bash highlighter.
+              aliases: {},
+              // This toggles the display of line numbers globally alongside the code.
+              // To use it, add the following line in src/layouts/index.js
+              // right after importing the prism color scheme:
+              //  `require("prismjs/plugins/line-numbers/prism-line-numbers.css");`
+              // Defaults to false.
+              // If you wish to only show line numbers on certain code blocks,
+              // leave false and use the {numberLines: true} syntax below
+              showLineNumbers: false,
+              // If setting this to true, the parser won't handle and highlight inline
+              // code used in markdown i.e. single backtick code like `this`.
+              noInlineHighlight: false
+            }
           },
           {
             resolve: `gatsby-remark-images`,
@@ -62,16 +98,16 @@ module.exports = {
               // It's important to specify the maxWidth (in pixels) of
               // the content container as this plugin uses this as the
               // base for generating different widths of each image.
-              maxWidth: 590,
-            },
-          },
-        ],
-      },
+              maxWidth: 590
+            }
+          }
+        ]
+      }
     },
-    `gatsby-plugin-remove-trailing-slashes`,
+    `gatsby-plugin-remove-trailing-slashes`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
   ],
-  pathPrefix: '/',
+  pathPrefix: '/'
 }
